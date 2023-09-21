98f4719bba7a722b35a63f86e4af35767363fc60
Minh Anthony Tat <minhanthonytat@gmail.com>
2023-09-21 16:52:19 +1000

    removed key

--- google-book/src/services/google-books.js
+++ google-book/src/services/google-books.js
@@ -1,11 +1,10 @@
 export const getBooksBySearchTerm = async (seachTerm = "") => {
-	const key = "AIzaSyBINqlWoas8k55XAAvtsGYD4z7FqwxkIQ0";
-	const response = await fetch(
-		`https://www.googleapis.com/books/v1/volumes?q=${seachTerm}&key=${key}`
-	);
-	if (!response.ok) {
-		throw new Error("Failed to fetch book search");
-	}
-	const data = await response.json();
-	return data.items;
+  const response = await fetch(
+    `https://www.googleapis.com/books/v1/volumes?q=${seachTerm}`
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch book search");
+  }
+  const data = await response.json();
+  return data.items;
 };
